c068e541d97c33dce7eb079bd183ff1ccceecf00
medvedevaviktoria <161297736+medvedevaviktoria@users.noreply.github.com>
2024-12-03 16:19:47 +0300

    Add files via upload

---
+++ UnitTests/UnitTests.cpp
@@ -0,0 +1,23 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../kr/dates.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTests
+{
+	TEST_CLASS(UnitTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethodMonthDays)
+		{
+			int month = 12;
+			int year = 2024;
+			int expected = 31;
+			int actual = monthDays(month, year);
+
+			Assert::AreEqual(expected, actual);
+		}
+	};
+}
